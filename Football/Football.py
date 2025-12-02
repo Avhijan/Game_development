43fca6a696bcebfd66be9da0fa5b170b3d83bc78
Avhijan <abhijansap06@gmail.com>
2025-12-02 22:02:09 +0545

    Update Football.py
    Addition of Football shape with very basic movement and colored characters to distinguish between players.

--- Football/Football.py
+++ Football/Football.py
@@ -11,12 +11,16 @@ pygame.display.set_caption("Football")
 white=(255,255,255)
 black=(0,0,0)
 red=(255, 0 ,0)
+blue=(0,0,255)
+skin=(232, 190, 172)
 dark_green=(0,100,0)
 light_green=(144,238,144)
 
+fpsclock=pygame.time.Clock()
 #game varriables
 center=((screen_width/2,screen_height/2))
 pi=3.1415
+fps=30
 
 def draw_field():
     #for background grass
@@ -38,32 +42,82 @@ def draw_field():
     pygame.draw.arc(screen, white,(160-78, screen_height/2-70, 140, 140), 3*pi/2, pi/2, 10 )
     pygame.draw.arc(screen, white,(screen_width-160-70, screen_height/2-70, 140, 140), pi/2, 3*pi/2, 10 )
 
-
-class player:
+class football:
     def __init__(self,x,y):
         self.x=x
         self.y=y
+        self.ball_radius=30
+        self.speed_x = 5
+        self.speed_y = 5
         self.rect=Rect(self.x, self.y, 20, 30)
 
+    def draw_ball(self):
+        pygame.draw.circle(screen, white, (self.rect.x, self.rect.y), self.ball_radius)
+        pygame.draw.circle(screen, black, (self.rect.x, self.rect.y), self.ball_radius, 3)
+        # x = 8
+        # y = 10
+        # x1 = self.rect.x - x
+        # y1 = self.rect.y 
+
+        # x2 = self.rect.x + x
+        # y2 = self.rect.y 
+
+        # x3 = self.rect.x + x
+        # y3 = self.rect.y - y
+
+        # x4 = self.rect.x + x/2-0.5
+        # y4 = self.rect.y -2*y
+
+        # x5 = self.rect.x - x
+        # y5 = self.rect.y - y
+
+        #points=[(x1,y1),(x2,y2),(x3,y3),(x4,y4),(x5,y5)]
+        #pygame.draw.polygon(screen, black, [(x1,y1),(x2,y2),(x3,y3),(x4,y4),(x5,y5)])
+
+    def move_ball(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        
+
+class player:
+    def __init__(self,x,y,color):
+        self.x=x
+        self.y=y
+        self.player_color=color
+        self.rect=Rect(self.x, self.y, 20, 35)
+
     def draw_player(self):
-        pygame.draw.rect(screen, white, self.rect, 8,)
-        pygame.draw.circle(screen,white,(self.x+10,self.y-12), 14,7)
+        pygame.draw.rect(screen, black, self.rect)
+        pygame.draw.rect(screen,self.player_color , (self.x, self.y, 20, 20))
+        pygame.draw.circle(screen, skin, (self.x+10,self.y-14), 14)
+        pygame.draw.circle(screen, black, (self.x+10,self.y-14), 14, 1)
+        
 
 
 #player 
-player1 = player(screen_width-800,screen_height-40)
-player2 = player(screen_width-200, screen_height-40)
+player1 = player(screen_width-900,screen_height//2, blue)
+player2 = player(screen_width-100, screen_height//2, red)
+ball = football(screen_width//2, screen_height//2)
+
 
 running=True
 while running:
+    fpsclock.tick(fps)
     draw_field()
+     #draw player
+    player1.draw_player()
+    player2.draw_player()
+
+    #draw ball
+    ball.draw_ball()
+    #move football
+    ball.move_ball()
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
-
-        #draw player
-        player1.draw_player()
-        player2.draw_player()
 
         
 
